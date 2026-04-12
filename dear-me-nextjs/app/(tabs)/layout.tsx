f90c70beb788741b6ce84c874f872365afdc3070
diraj thajali <thajalidiraj@gmail.com>
2026-04-12 15:42:02 -0700

    feat(nextjs): add (tabs) layout with TabBar

---
+++ dear-me-nextjs/app/(tabs)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import { TabBar } from "@/components/dear-me/tab-bar";
+import { ScreenBackground } from "@/components/dear-me/screen-background";
+
+export default function TabsLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative flex min-h-dvh flex-col">
+      <ScreenBackground />
+      <div className="relative flex-1 overflow-y-auto pb-24">{children}</div>
+      <div className="absolute inset-x-0 bottom-0 px-4 pb-4">
+        <TabBar />
+      </div>
+    </div>
+  );
+}
